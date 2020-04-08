bfcc17476a9d43cfd1b574260198ba1db7ef715b
seu nome <fabioolivc@gmail.com>
2020-04-08 19:50:40 -0300

    Update2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  get 'inicio', to: 'welcome#index'
+  get 'teste', to: 'welcome#index'
+  root to: 'welcome#index'
   devise_for :users
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
