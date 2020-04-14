1ec16d48d545501643563b87ee6ee133c1a40a2a
Rick <rickmaxg3@hotmail.com>
2020-04-14 18:48:23 -0300

    fix welcome

--- app/controllers/admins_backoffice/welcome_controller.rb
+++ app/controllers/admins_backoffice/welcome_controller.rb
@@ -1,5 +1,10 @@
 class AdminsBackoffice::WelcomeController < AdminsBackofficeController
   
   def index
+    @admins = Admin.all
+  end
+
+  def edit
+    @admin = Admin.find(params[:id])
   end
 end
